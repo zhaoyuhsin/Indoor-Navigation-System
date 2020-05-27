38553e7e428defbafb2fd0ea9d06e868e651677b
YuHsin <zhaoyuxin1998@gmail.com>
2020-05-27 12:00:32 +0800

    upload slam module

---
+++ ORB_SLAM2/cmake-build-debug/CMakeFiles/bin_vocabulary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../Vocabulary/bin_vocabulary"
+  "../Vocabulary/bin_vocabulary.pdb"
+  "CMakeFiles/bin_vocabulary.dir/Vocabulary/bin_vocabulary.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin_vocabulary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ORB_SLAM2/cmake-build-debug/CMakeFiles/stereo_euroc.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yuhsin/project/Alkaid/ORB_SLAM2/Examples/Stereo/stereo_euroc.cc" "/home/yuhsin/project/Alkaid/ORB_SLAM2/cmake-build-debug/CMakeFiles/stereo_euroc.dir/Examples/Stereo/stereo_euroc.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/home/yuhsin/下载/cmake-3.16.1/Pangolin/include"
+  "/home/yuhsin/下载/cmake-3.16.1/Pangolin/build/src/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yuhsin/project/Alkaid/ORB_SLAM2/cmake-build-debug/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
